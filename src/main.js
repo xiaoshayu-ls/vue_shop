8b0910a908e211e4baaa1a27cdfb2319447711e9
1070773183@qq.com <1070773183@qq.com>
2021-02-02 10:08:41 +0800

    商品分类功能完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/font_2169052_afv0ky5xejc/iconfont.css'
-
+import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use((config) => {
@@ -21,7 +21,7 @@ axios.interceptors.response.use((res) => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: (h) => h(App)
